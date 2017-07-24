013b4a566be94cfdf140b8bc31bb478210848958
Shanakor <rammerstorfer.n@gmail.com>
2017-07-24 17:42:47 +0200

    Did the first steps of connecting the ui with the code.

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var recordingLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +21,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func recordAudio(_ sender: Any) {
+        print("record button was pressed!")
+        recordingLabel.text = "Recording in Progress"
+    }
 
 }
 
