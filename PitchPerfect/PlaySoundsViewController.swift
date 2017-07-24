4ea23723f728df02de543904478429c0b1caae0b
Shanakor <rammerstorfer.n@gmail.com>
2017-07-24 19:45:25 +0200

    Implemented receiving of the recorded audio url in the PlaySoundsViewController

---
+++ PitchPerfect/PlaySoundsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PlaySoundsViewController.swift
+//  PitchPerfect
+//
+//  Created by Niklas Rammerstorfer on 24/07/2017.
+//  Copyright © 2017 rammerstorfer. All rights reserved.
+//
+
+import UIKit
+
+class PlaySoundsViewController: UIViewController {
+    var recordedAudioURL: URL!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
