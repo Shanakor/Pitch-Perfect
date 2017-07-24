4df08e7e521c33fc3fbb7d6f14bed15fad7e3249
Shanakor <rammerstorfer.n@gmail.com>
2017-07-24 18:54:22 +0200

    Reworked the user interface too look awesome (with designer icons now)

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -10,24 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        stopRecordingButton.isEnabled = false
     }
 
     @IBAction func recordAudio(_ sender: Any) {
-        print("record button was pressed!")
         recordingLabel.text = "Recording in Progress"
+        
+        recordButton.isEnabled = false
+        stopRecordingButton.isEnabled = true
     }
 
     @IBAction func stopRecording(_ sender: Any) {
-        print("stop recording button was pressed!")
+        recordButton.isEnabled = true
+        stopRecordingButton.isEnabled = false
     }
 }
 
